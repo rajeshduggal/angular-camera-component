feffa028e6ed73032fbb412a61917d496322676a
Rajesh Duggal <rajesh@duggalmail.com>
2018-07-03 23:32:01 -0400

    ng generate component camera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CameraComponent } from './camera/camera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent
   ],
   imports: [
     BrowserModule
