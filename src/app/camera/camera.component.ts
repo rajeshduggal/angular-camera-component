020d90c58d0cd20c25ea7e44bd118b5824e0cb04
Rajesh Duggal <rajesh@duggalmail.com>
2018-07-04 03:08:10 -0400

    Add video stream into CameraComponent

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.css']
 })
-export class CameraComponent implements OnInit {
+export class CameraComponent implements AfterViewInit {
+
+  @ViewChild('video') video;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const videoElement: HTMLVideoElement = this.video.nativeElement;
+    navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'user'}
+    }).then(stream => {
+      videoElement.srcObject = stream;
+    });
   }
 
 }
